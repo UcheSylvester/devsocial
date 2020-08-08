9c850bc3cbc1b17ba6b127f6c8b76101ee22baa3
UcheSylvester <okorocode@gmail.com>
2020-08-08 04:11:22 +0100

    initial setup done

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get("/", (req, res) => res.send("HELLO!"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
