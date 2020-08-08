653b54df9ac3122d243185fb0a864bab285cb563
UcheSylvester <okorocode@gmail.com>
2020-08-08 04:46:31 +0100

    done setting up mongooese

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://sylvester:sylvester@cluster0.mlcle.mongodb.net/devsocial?retryWrites=true&w=majority",
+};
--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const keys = require("./config/keys");
 
 const app = express();
+
+// DB CONFIG
+const db = keys.mongoURI;
+
+// CONNECT TO MONGODB
+mongoose
+  .connect(db, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then((res) => console.log("DB connnected successfully!", res))
+  .catch((err) => console.log("DB connection failed"));
 
 app.get("/", (req, res) => res.send("HELLO!"));
 
